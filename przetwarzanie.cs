bea3422be8684b9676c8770b285177e83632683b
Bartek_laptop <b748462847@gmail.com>
2025-04-21 16:40:20 +0200

    Brakujący plik dodany "przetwarzanie"

---
+++ przetwarzanie.cs
@@ -0,0 +1,102 @@
+using System.Reflection.Metadata;
+
+namespace ParallelWatki
+{
+    public partial class Form3 : Form
+    {
+        private void progowanie() {
+            Bitmap ObrazBitmap = new Bitmap(pictureBoxOriginal.Image !);
+            int width = ObrazBitmap.Width;
+            int height = ObrazBitmap.Height;
+
+            for(int h = 0; h < height; ++h) {
+                for(int w = 0; w < width; ++w) {
+                    Color oryginalKolor = ObrazBitmap.GetPixel(w, h);
+                    double Luminancja = 0.3*oryginalKolor.R + 0.59*oryginalKolor.G + 0.11*oryginalKolor.B;
+                    Color progowanieKolor = (Luminancja > 128) ? Color.White : Color.Black;
+                    ObrazBitmap.SetPixel(w, h, progowanieKolor);
+                }
+            }
+            pictureBoxResult1.Image = ObrazBitmap;
+        }
+
+
+
+        private void krawedz() {
+            Bitmap ObrazBitmap = new Bitmap(pictureBoxOriginal.Image !);
+            int width = ObrazBitmap.Width;
+            int height = ObrazBitmap.Height;
+            Bitmap krawdzBitmap = new Bitmap(width, height);
+            int[,] sobelX = new int[,] {
+                { -1, 0, 1 },
+                { -2, 0, 2 },
+                { -1, 0, 1 }
+            };
+            int[,] sobelY = new int[,] {
+                { -1, -2, -1 },
+                {  0,  0,  0 },
+                {  1,  2,  1 }
+            };
+
+            for(int h = 1; h < height-1; ++h) {
+                for(int w = 1; w < width-1; ++w) {
+                    int gx = 0;
+                    int gy = 0;
+                    for (int ky = -1; ky <= 1; ky++) {
+                        for (int kx = -1; kx <= 1; kx++) {
+                            Color pixelColor = ObrazBitmap.GetPixel(w + kx, h + ky);
+                            int gray = (int)((pixelColor.R + pixelColor.G + pixelColor.B) / 3);
+
+                            gx += sobelX[ky + 1, kx + 1] * gray;
+                            gy += sobelY[ky + 1, kx + 1] * gray;
+                        }
+                    }
+
+                    int magnitude = (int)Math.Sqrt(gx * gx + gy * gy);
+                    magnitude = Math.Clamp(magnitude, 0, 255);
+
+                    Color edgeColor = Color.FromArgb(magnitude, magnitude, magnitude);
+                    krawdzBitmap.SetPixel(w, h, edgeColor);
+                }
+            }
+            pictureBoxResult2.Image = krawdzBitmap;
+        }
+
+
+
+        private void negatyw() {
+            Bitmap ObrazBitmap = new Bitmap(pictureBoxOriginal.Image !);
+            int width = ObrazBitmap.Width;
+            int height = ObrazBitmap.Height;
+
+            for(int h = 0; h < height; ++h) {
+                for(int w = 0; w < width; ++w) {
+                    Color oryginalKolor = ObrazBitmap.GetPixel(w, h);
+                    int R = 255 - oryginalKolor.R;
+                    int G = 255 - oryginalKolor.G;
+                    int B = 255 - oryginalKolor.B;
+                    Color negatywKolor = Color.FromArgb(oryginalKolor.A, R, G , B);
+                    ObrazBitmap.SetPixel(w, h, negatywKolor);
+                }
+            }
+            pictureBoxResult3.Image = ObrazBitmap;
+        }
+
+
+
+        private void lustrzane() { 
+            Bitmap ObrazBitmap = new Bitmap(pictureBoxOriginal.Image !);
+            int width = ObrazBitmap.Width;
+            int height = ObrazBitmap.Height;
+            Bitmap lustrzanyBitmap = new Bitmap(width, height);
+
+            for(int h = 0; h < height; ++h) {
+                for(int w = 0; w < width; ++w) {
+                    Color lustrzanyKolor = ObrazBitmap.GetPixel(width-w-1, h);
+                    lustrzanyBitmap.SetPixel(w, h, lustrzanyKolor);
+                }
+            }
+            pictureBoxResult4.Image = lustrzanyBitmap;
+        }
+    }
+}
